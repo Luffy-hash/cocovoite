eddb82e8971f220e38a8a510f7fdca9ab0f9ce50
ABDOULAYE BARRY <lawt71792@gmail.com>
2025-11-18 10:30:26 +0100

    initial projects cocovoite

---
+++ cocovoite_server/src/main/java/com/example/cocovoite_server/CocovoiteServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cocovoite_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocovoiteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocovoiteServerApplication.class, args);
+	}
+
+}
---
+++ cocovoite_server/src/test/java/com/example/cocovoite_server/CocovoiteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cocovoite_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocovoiteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
